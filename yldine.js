7ba9a4603a5e58ca3b2577f8e6baa680f382cc76
Markkus Koddala <markkusk@ut.ee>
2022-12-05 12:05:37 +0200

    uuendus

--- yldine.js
+++ yldine.js
@@ -1,3 +1,5 @@
+/* switcheri idee ja css, html ja javasripti koodi sain youtube videost https://www.youtube.com/watch?v=N3-K5G2qFDM, kus on ka link allikani´https://github.com/lashaNoz/Dark-Mode */
+
 const container = document.querySelector(".navbar");
 const container2 = document.querySelector(".toggle");
 const toggle = document.querySelector(".toggle");
