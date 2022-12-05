56554c4686224096562c58d6bfed8c491965043d
username <lotte@Liisa-Lotte-MacBook-Pro.local>
2022-12-05 16:07:41 +0200

    js kommentaarid

--- js/JS_õpetajatelehel.js
+++ js/JS_õpetajatelehel.js
@@ -1,21 +1,23 @@
 /* Javascripti koodi sain veebilehelt https://www.w3schools.com/howto/howto_js_read_more.asp */
 
+/*Funktsioon, loe rohkem/loe vähem nupu toimimiseks*/
 function myFunction(){
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
+    var dots = document.getElementById("dots"); /* otsib üles, kus tuleb asendada tekst kolme täpiga*/
+    var moreText = document.getElementById("more"); /*tähistab teksti, mis on peidetud enne nupule vajutamist*/
+    var btnText = document.getElementById("myBtn"); /* tähistab nuppu, millele vajutades hakkab funktsioon tööle*/
   
-    if (dots.style.display === "none") {
+    if (dots.style.display === "none") { /*kui nuppu ei ole vajutatud*/
       dots.style.display = "inline";
       btnText.innerHTML = "Loe rohkem"; 
       moreText.style.display = "none";
-    } else {
+    } else {/*kui nupp on vajutatud*/
       dots.style.display = "none";
       btnText.innerHTML = "Loe vähem"; 
       moreText.style.display = "inline";
     }
   }
 
+/*Samad tegevused, mis eelnevas funktsioonis*/
   function myFunction2(){
     var dots = document.getElementById("dots2");
     var moreText = document.getElementById("more2");
