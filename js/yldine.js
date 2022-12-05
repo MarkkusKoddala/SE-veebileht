fc52eca6d5f8e0ccce9a3fb89e884575d15bacd1
Markkus Koddala <markkusk@ut.ee>
2022-12-05 17:06:12 +0200

    uuendus

--- js/yldine.js
+++ js/yldine.js
@@ -1,10 +1,10 @@
 /* switcheri idee ja css, html ja javasripti koodi sain youtube videost https://www.youtube.com/watch?v=N3-K5G2qFDM, kus on ka link allikani´https://github.com/lashaNoz/Dark-Mode */
 
-const container = document.querySelector(".navbar");
-const container2 = document.querySelector(".toggle");
-const toggle = document.querySelector(".toggle");
+const container = document.querySelector(".navbar");/* loob konstanti, mis saab väärtuseks elemendi, millega matchivad ".navbar" selectorid*/
+const container2 = document.querySelector(".toggle");/* kordub sama*/
+const toggle = document.querySelector(".toggle");/* kordub sama*/
 
-toggle.addEventListener("click", () => {
+toggle.addEventListener("click", () => { /*spetsiifilise eventi korral funktsioon käivitub ja vastavad väärtused .css failis muudetakse (värvid) */
   container.classList.toggle("dark")
   container2.classList.toggle("dark")
     ? (toggle.firstElementChild.className = "fa-solid fa-brush")
